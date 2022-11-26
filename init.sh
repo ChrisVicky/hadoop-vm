7a63b0e43be2f3e5ca856e4b2d6707b509479f2f
Christopher <1051666563@qq.com>
2022-11-26 15:36:37 +0800

    tmp

--- init.sh
+++ init.sh
@@ -14,25 +14,22 @@ cp /vagrant/cache/sources.list /etc/apt/sources.list
 ufw disable
 
 # Java1.8 Configuration
-cd /usr/local
-mkdir java
+mkdir /usr/local/java
 tar -zxf /vagrant/cache/jdk-8u161-linux-x64.tar.gz -C /usr/local/java
 echo "export JAVA_HOME=/usr/local/java/jdk1.8.0_161" >> /etc/profile
 echo "export CLASSPATH=.:\$JAVA_HOME/lib:\$JAVA_HOME/jre/lib:\$CLASSPATH" >> /etc/profile
 echo "export PATH=\$JAVA_HOME/bin:\$JAVA_HOME/jre/bin:\$PATH" >> /etc/profile
 
 # Scala Configuration
-cd /usr/local
 tar -zxf /vagrant/cache/scala-2.11.8.tgz -C /usr/local
-mv ./scala-2.11.8 ./scala
+mv /usr/local/scala-2.11.8 /usr/local/scala
 echo "export SCALA_HOME=/usr/local/scala" >> /etc/profile
 source /etc/profile
 echo "export PATH=\$SCALA_HOME/bin:\$PATH" >> /etc/profile
 
 # Hadoop configuration
-cd /usr/local
 tar -zxf /vagrant/cache/hadoop-2.9.0.tar.gz -C /usr/local
-mv ./hadoop-2.9.0 ./hadoop
+mv /usr/local/hadoop-2.9.0 /usr/local/hadoop
 echo "export HADOOP_HOME=/usr/local/hadoop" >> /etc/profile
 source /etc/profile
 echo "export PATH=\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin:\$JAVA_HOME/bin:\$PATH" >> /etc/profile
@@ -43,9 +40,9 @@ echo "slave1" > /usr/local/hadoop/etc/hadoop/slaves
 echo "slave2" >> /usr/local/hadoop/etc/hadoop/slaves
 
 cd /usr/local/hadoop
-mkdir ./dfs
-mkdir ./dfs/name ./dfs/data 
-mkdir ./tmp
+mkdir /usr/local/hadoop/dfs
+mkdir /usr/local/hadoop/dfs/name /usr/local/hadoop/dfs/data 
+mkdir /usr/local/hadoop/tmp
 
 # User Interfaces -- Ports can be found in those files
 cp /vagrant/cache/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
